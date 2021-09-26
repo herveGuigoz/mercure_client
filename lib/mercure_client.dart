c7d81bcd415e06da43337981e5beadb52971b360
herveGuigoz <herve.guigoz@gmail.com>
2021-09-26 22:12:16 +0200

    🔥 refactor: mercure

--- lib/mercure_client.dart
+++ lib/mercure_client.dart
@@ -1,6 +1,5 @@
 library mercure_client;
 
-export 'src/mercure.dart';
-export 'src/mercure_client.dart';
+export 'src/mercure.dart' show Mercure;
 export 'src/mercure_error.dart';
 export 'src/mercure_event.dart';
--- lib/src/mercure_error.dart
+++ lib/src/mercure_error.dart
@@ -1,19 +1,53 @@
-import 'package:dio/dio.dart';
-
-/// {@template mercure_client.MercureError}
-/// Exception when request failed or for malformed event.
+/// {@template mercure_client.mercure_exception}
+/// Exception when response failed with bad status code.
 /// {@endtemplate}
-class MercureError implements Exception {
-  /// {@macro mercure_client.MercureError}
-  MercureError({
-    this.response,
-    this.error,
-  });
+class MercureException implements Exception {
+  /// {@macro mercure_client.mercure_exception}
+  MercureException(this.statusCode, this.uri);
 
-  /// Response info, it may be `null` if the request can't reach to
-  /// the http server, for example, occurring a dns error, network is not available.
-  Response? response;
+  /// The response status code
+  final int statusCode;
 
-  /// The error/exception object
-  dynamic error;
+  /// The request uri
+  final String uri;
+
+  @override
+  String toString() {
+    return 'Connection failed with $statusCode status code for $uri';
+  }
+}
+
+/// {@template mercure_client.content_type_exception}
+/// Exception when response return unexpected content type header.
+/// {@endtemplate}
+class ContentTypeException implements Exception {
+  /// {@macro mercure_client.content_type_exception}
+  const ContentTypeException(this.contentType, this.uri);
+
+  /// The actual content type header returned by the response.
+  final String contentType;
+
+  /// The request uri
+  final String uri;
+
+  @override
+  String toString() {
+    return 'Response content-type is $contentType while "text/event-stream" was expected for $uri';
+  }
+}
+
+/// {@template mercure_client.mercure_event_excepion}
+/// Exception when server returned malformed event.
+/// {@endtemplate}
+class MercureEventException implements Exception {
+  /// {@macro mercure_client.mercure_event_excepion}
+  MercureEventException(this.event);
+
+  /// The event recieved by the server.
+  final String event;
+
+  @override
+  String toString() {
+    return 'Invalid event $event';
+  }
 }
